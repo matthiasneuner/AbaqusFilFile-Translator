655ae551846dbb3f8decc66bbc6ee19a053c166e
MatthiasN <matthias.neuner@uibk.ac.at>
2017-04-06 11:56:39 +0200

    working prototype

--- filConverter.py
+++ filConverter.py
@@ -5,6 +5,7 @@ import math
 
 import utils.exportEngine as eE
 from utils.inputfileparser import parseInputFile, printKeywords
+import time
 
 if __name__ == "__main__":
     
@@ -14,6 +15,7 @@ if __name__ == "__main__":
         print('Available Keywords:')
         print('')
         printKeywords()
+        exit()
         
     fn = sys.argv[1]
     jobFile = sys.argv[2]
@@ -39,41 +41,64 @@ if __name__ == "__main__":
     
     fileIdx = 0
     wordIdx = 0
-    fn = np.memmap(fn, dtype='b', mode='r+', )
-    while fileIdx < fileSize:
-        
-        fileRemainder = fileSize - fileIdx # remaining file size in BYTES
-        idxEnd = fileIdx + (batchSize if fileRemainder >= batchSize else fileRemainder) #get end index 
-        batchChunk = np.copy(fn[fileIdx :   idxEnd])  # get chunk of file
-        words = batchChunk.reshape( -1 , chunkSize )  # get words 
-        words = words[:, 4:-4]
-        words = words.reshape(-1, 8)
-        while wordIdx < len(words):
-            recordLength = eE.filInt(words[wordIdx])[0]  
-            if recordLength<=2:
-                print('found a record with 0 length content, possible an aborted Abaqus analysis')
-                break
+
+    
+#    while fileIdx < fileSize:
+    while True:
+        try:
+            fileStat = os.stat(fn)
+            fileSize = fileStat.st_size
             
-            if wordIdx + recordLength > len(words):
-                bytesProgressedInCurrentBatch = int(math.floor(wordIdx/512))* 513 * 8
-                if bytesProgressedInCurrentBatch == 0: # indicator for an aborted analysis
-                    print('terminated file, possible an aborted Abaqus analysis')
-                    fileIdx = fileSize
-                    break
-                fileIdx +=   bytesProgressedInCurrentBatch# move to beginning of the current 512 word block in the batchChunk and restart with a new bathChunk
-                wordIdx =  ( (wordIdx%512) )                  # of course, restart at the present index
-                break
+            if fileIdx < fileSize:
+                fnMap = np.memmap(fn, dtype='b', mode='r', )
+                fileRemainder = fileSize - fileIdx # remaining file size in BYTES
+                idxEnd = fileIdx + (batchSize if fileRemainder >= batchSize else fileRemainder) #get end index 
+                
+                # in case we are operating on an unfinished file and 'catch' an unfinished chunk
+                idxEnd -= idxEnd % chunkSize
+                                   
+                batchChunk = np.copy(fnMap[fileIdx :   idxEnd])  # get chunk of file
+                words = batchChunk.reshape( -1 , chunkSize )  # get words 
+                words = words[:, 4:-4]
+                words = words.reshape(-1, 8)
+                while wordIdx < len(words):
+                    recordLength = eE.filInt(words[wordIdx])[0]  
+                    if recordLength<=2:
+                        print('found a record with 0 length content, possible an aborted Abaqus analysis')
+                        break
+                    
+                    # the next record exceeds our batchChunk, so we do some trick:
+                        # - set the wordIdx to the end of the so far progressed frame
+                        # - move the frame to the wordIDx
+                    if wordIdx + recordLength > len(words):
+                        bytesProgressedInCurrentBatch = int(math.floor(wordIdx/512))* 513 * 8
+                        if bytesProgressedInCurrentBatch == 0: # indicator for an aborted analysis
+                            print('terminated file, possible an aborted Abaqus analysis')
+                            fileIdx = fileSize
+                            break
+                        fileIdx +=   bytesProgressedInCurrentBatch# move to beginning of the current 512 word block in the batchChunk and restart with a new bathChunk
+                        wordIdx =  ( (wordIdx%512) )                  # of course, restart at the present index
+                        break
+                    
+                    recordType = eE.filInt(words[wordIdx+1])[0]
+                    recordContent = words[wordIdx+2 : wordIdx+recordLength]
+                    success = exportEngine.computeRecord(recordLength, recordType, recordContent)
+                    wordIdx += recordLength
+                    
+                # clean finish of a batchChunk
+                if wordIdx == len(words):
+                    wordIdx = 0
+                    fileIdx = idxEnd
+#                    fileIdx += batchSize
+                    
+                del words
+            else:
+                print("waiting for new result .fil data or CTRL-C to finish...")
+                time.sleep(10)
             
-            recordType = eE.filInt(words[wordIdx+1])[0]
-            recordContent = words[wordIdx+2 : wordIdx+recordLength]
-            success = exportEngine.computeRecord(recordLength, recordType, recordContent)
-            wordIdx += recordLength
-            
-        if wordIdx == len(words):
-            wordIdx = 0
-            fileIdx += batchSize
-            
-        del words
+        except KeyboardInterrupt:
+            print("Interrupted by user")
+            break
             
     exportEngine.finalize()
         
